de24302110808b4e99373a5e7b425874d6a625f9
lirosm <>
2023-03-13 19:18:48 +0100

    Constructing layout

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { Home, Contact, Cart, Nav } from "./components/navigation/nav";
+import { Layout } from "./components/layout/layout";
+import { Home, Contact, Cart } from "./components/navigation/nav";
 
 function App() {
   return (
   <div>
-    <Nav />
     <Routes>
-      <Route index element={<Home />}/>
-      <Route path="contact" element={<Contact />} />
-      <Route path="cart" element={<Cart />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />}/>
+        <Route path="contact" element={<Contact />} />
+        <Route path="cart" element={<Cart />} />
+      </Route>
     </Routes>
   </div>
   )
