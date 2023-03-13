2565b30537d9a35664587e281eef70263b13ca9f
lirosm <>
2023-03-13 19:07:46 +0100

    basic navigation

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home, Contact, Cart, Nav } from "./components/navigation/nav";
 
 function App() {
   return (
   <div>
-    <div>Hello from the other side</div>
+    <Nav />
+    <Routes>
+      <Route index element={<Home />}/>
+      <Route path="contact" element={<Contact />} />
+      <Route path="cart" element={<Cart />} />
+    </Routes>
   </div>
   )
 }
---
+++ ecommerce-app/src/components/navigation/nav.js
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom'
+
+export function Home() {
+    return <div>Home</div>
+}
+
+export function Contact() {
+    return <div>Contact</div>
+}
+
+export function Cart() {
+    return <div>Cart</div>
+}
+
+export function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/contact'>Contact</Link>
+                </li>
+                <li>
+                    <Link to='/cart'>Cart</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
