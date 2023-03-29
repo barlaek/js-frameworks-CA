a42b1da119d16f018091d50a3489cb2177be18ff
Liridona <91525128+barlaek@users.noreply.github.com>
2023-03-29 19:48:39 +0200

    Cart component added

---
+++ ecommerce-app/src/components/cart/cart.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Cart = ({ cartItems }) => {
+    const totalPrice = cartItems.reduce((acc, cur) => acc + cur.price * cur.quantity );
+
+    return (
+        <div>
+            <h2>Cart</h2>
+            <ul>
+                {cartItems.map(item => {
+                    <li key={item.id}>
+                        {item.title} - {item.quantity} x ${item.price}
+                    </li>
+                })}
+            </ul>
+            <p>Total: ${totalPrice}</p>
+        </div>
+    )
+}
+
+export default Cart;
