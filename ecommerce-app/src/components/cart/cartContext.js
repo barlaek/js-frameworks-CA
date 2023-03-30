69a31de8e1fe814fe45f9aec224e3c423a7f1e8d
Liridona <91525128+barlaek@users.noreply.github.com>
2023-03-30 21:07:19 +0200

    cleanup

--- ecommerce-app/src/components/cart/cartContext.js
+++ ecommerce-app/src/components/cart/cartContext.js
@@ -2,10 +2,10 @@ import React, { createContext, useContext, useReducer } from "react";
 import { Api } from "../api/api";
 import { reducer } from "./reducer";
 
-export const Cart = createContext();
+export const Cart = createContext(null);
 
 const CartContext = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, GetProducts)
+    const [state, dispatch] = useReducer(reducer, )
 
     return (
         <Cart.Provider value={{state, dispatch}}>
@@ -19,34 +19,3 @@ export default CartContext;
 export const useCartState = () => {
     return useContext(Cart);
 }
-
-export const GetProducts = () => {
-    const { data, isLoading, isError } = Api(
-        "https://api.noroff.dev/api/v1/online-shop"
-      );
-
-      if(isLoading) {
-        return <div>Loading</div>
-      }
-
-      if(isError) {
-        return <div>Error</div>
-      }
-    
-    let productData = data.find(product => product.id)
-    // const products = [...data].map((product) => ({
-    //     title: product.title,
-    //     id: product.id,
-    //     image: product.imageUrl,
-    //     price: product.price,
-    // }))
-
-    console.log(productData)
-
-    return productData;
-}
-
-const { products } = GetProducts;
-const initialState = { products: products, cart: [] }
-
-console.log(initialState)
--- ecommerce-app/src/components/products/products.js
+++ ecommerce-app/src/components/products/products.js
@@ -1,37 +1,5 @@
-// import React, { useContext } from "react";
+
 import { Link } from "react-router-dom";
-// import { Api } from "../api/api";
-
-
-// export function Products() {
-//     const { data, isLoading, isError } = Api(
-//         "https://api.noroff.dev/api/v1/online-shop"
-//       );
-    
-//       if (isLoading) {
-//         return <div>isLoading</div>
-//       }
-    
-//       if (isError) {
-//         return (
-//           <div>
-//             <div>Error</div>
-//           </div>
-//         )
-//       }
-      
-//     return (
-//         <div>
-//             {data.map((product) => (
-//                 <Link to ={`/${product.id}`}>
-//                     <div>{product.title}</div>
-//                     <img src={product.imageUrl} alt="product" />
-//                     <div>{product.price}</div>
-//                 </Link>
-//             ))}
-//         </div>
-//     )
-// }
 
 export function Products(props) {
     const product = props.product
