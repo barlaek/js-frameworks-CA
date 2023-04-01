133f192677c056b42ddf78867f292a69e47c10e8
Liridona <91525128+barlaek@users.noreply.github.com>
2023-04-01 12:58:43 +0200

    Total NaN, checkout and reducer

--- ecommerce-app/src/components/cart/cart.js
+++ ecommerce-app/src/components/cart/cart.js
@@ -9,12 +9,24 @@ export function CartItems() {
         <div key={state.state}>
             {state.state.cart.map((prod) => (
                 <div>
-                    <img src={prod.imageUrl} alt="product" />
-                    <h3>{prod.title}</h3>
-                    <p>{prod.quanitity}</p>
-                    <p>{prod.price}</p>
+                    <div>
+                        <img src={prod.imageUrl} alt="product" />
+                        <h3>{prod.title}</h3>
+                        <p>{prod.price}</p>
+                    </div>
+                    <div>
+                        <button onClick={() => state.dispatch({type: 'removeProduct', payload: prod.id})}>-</button>
+                        <p>{prod.quanitity}</p>
+                        <button onClick={() => state.dispatch({type: 'addProduct', payload: prod.id})}>+</button>
+                    </div>
                 </div>
             ))}
+            <div key={state.state.total}>
+                <div>
+                    <h3>Total: {state.state.total}</h3>
+                    <button>Checkout</button>
+                </div>
+            </div>
         </div>
     )
 }
