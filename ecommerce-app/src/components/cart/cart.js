bc53d4f599d8f763c93829912678d7a88ee423c8
Liridona <91525128+barlaek@users.noreply.github.com>
2023-04-01 12:32:26 +0200

    cart ui

--- ecommerce-app/src/components/cart/cart.js
+++ ecommerce-app/src/components/cart/cart.js
@@ -8,7 +8,12 @@ export function CartItems() {
     return (
         <div key={state.state}>
             {state.state.cart.map((prod) => (
-                <div>{prod.title}</div>
+                <div>
+                    <img src={prod.imageUrl} alt="product" />
+                    <h3>{prod.title}</h3>
+                    <p>{prod.quanitity}</p>
+                    <p>{prod.price}</p>
+                </div>
             ))}
         </div>
     )
