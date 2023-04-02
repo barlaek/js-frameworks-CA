aeb5e5c7c33edb71319456640613cc480880c93e
Liridona <91525128+barlaek@users.noreply.github.com>
2023-04-02 17:05:13 +0200

    home grid

--- ecommerce-app/src/components/products/products.js
+++ ecommerce-app/src/components/products/products.js
@@ -6,13 +6,14 @@ export function Products(props) {
 
     return (
         <div className={styles.card}>
-            <div>
-                <img className={styles.image} src={product.imageUrl} />
-            </div>
-            <div>
-                <h2>{product.title}</h2>
-                <p>{product.price}</p>
-                <Link to={`/${product.id}`}><button>View product</button></Link>
+            <img className={styles.image} src={product.imageUrl} />
+            <div className={styles.cardContent}>
+                <div className={styles.textBox}>
+                    <h3 className={styles.cardText}>{product.title}</h3>
+                    <p className={styles.cardText}>Rated: {product.rating}/5</p>
+                    <p className={styles.cardText}>Price: {product.price}kr</p>
+                </div>
+                <Link to={`/${product.id}`}><button className={styles.cardBtn}>View product</button></Link>
             </div>
         </div>
     )
