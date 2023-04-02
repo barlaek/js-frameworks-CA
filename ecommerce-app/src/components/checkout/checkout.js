0c5420c880bee588c5c18b1b8a5a3523f5f1a7a0
Liridona <91525128+barlaek@users.noreply.github.com>
2023-04-02 18:33:45 +0200

    checkout styles

--- ecommerce-app/src/components/checkout/checkout.js
+++ ecommerce-app/src/components/checkout/checkout.js
@@ -1,29 +1,33 @@
 import { useCartState } from "../cart/cartContext"
 import { Link } from "react-router-dom";
+import styles from './Checkout.module.css'
+import button from '../products/Product.module.css'
 
 export function CheckoutPage() {
     const state = useCartState();
 
     return (
-        <div>
+        <div className={styles.checkoutContainer}>
             <div>
                 <h1>Thank you for your purchase!</h1>
             </div>
-            <div>
+            <div className={styles.continue}>
                 <h2>Continue shopping?</h2>
-                <Link to="/"><button onClick={() => state.dispatch({type: 'clearCart'})}>Take me home</button></Link>
+                <Link to="/"><button onClick={() => state.dispatch({type: 'clearCart'})} className={button.cardBtn}>Take me home</button></Link>
             </div>
             <div>
                 <h3> Your receipt:</h3>
                 <div key={state.state}>
                     {state.state.cart.map((prod) => (
                     <ul>
-                        <li>Total: {state.state.total}kr</li>
-                        <li key={prod.id}>
-                            <img src={prod.imageUrl} alt="product" />
-                            <h3>{prod.title}</h3>
-                            <p>{prod.price}</p>
-                            <p>{prod.quantity}</p>
+                        <li key={prod.id} className={styles.receipt}>
+                            <img src={prod.imageUrl} alt="product" className={styles.receiptImg}/>
+                            <div className={styles.receiptContent}>
+                                <h3>Total: {state.state.total}kr</h3>
+                                <h4>{prod.title}</h4>
+                                <p>Price: {prod.price}</p>
+                                <p>Quantity: {prod.quantity}</p>
+                            </div>
                         </li>
                     </ul>
                     ))};
